6d437b3051d50d14b9a787e5418b13bac533a5a6
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-09-08 20:25:37 +0700

    feat: create message page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,37 @@
-import Image from "next/image";
+import Link from 'next/link'
+import React from 'react'
 
-export default function Home() {
+const LandingPage = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="w-full max-w-lg mx-auto min-h-screen flex flex-col items-start justify-center p-4">
+      <header className="mb-8">
+        <h1 className="text-xl font-bold">
+          Ngifent Hiatus – Terima Kasih Banyak! 🙏🎉
+        </h1>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <section className="max-w-xl space-y-4">
+        <p>Hai semuanya!</p>
+        <p>
+          Kami mau mengucapkan terima kasih yang sebesar-besarnya buat kalian yang udah percaya dan pakai <strong>Ngifent</strong> sebagai platform e-ticketing event komunitas kalian.
+        </p>
+        <p>
+          Mulai tanggal <strong>27 Agustus 2024</strong>, server Ngifent akan berhenti dulu 😔 dan sementara ini belum ada rencana buat lanjut pengembangan lagi. Kami bangga banget bisa bantu <strong>459 pengguna</strong> dan jadi bagian dari <strong>3 event</strong> keren yang pakai Ngifent sebagai platform mereka.
+        </p>
+        <p>
+          Meskipun harus hiatus sekarang, kami tetap berharap suatu saat nanti Ngifent bisa balik lagi dengan layanan yang lebih oke buat bantu event kalian jadi lebih mudah dan seru 🎫✨.
+        </p>
+        <p>Sekali lagi, terima kasih atas semua dukungannya! 💖</p>
+      </section>
+
+      <footer className="mt-8">
+        <p>Sampai jumpa lagi,</p>
+        <Link href='https://www.linkedin.com/in/hidayahapriliansyah/' target='_blank' className='hover:underline'>
+          <strong>Ngifent - Adi Muhamad Firmansyah</strong>
+        </Link>
       </footer>
-    </div>
-  );
+    </main>
+  )
 }
+
+export default LandingPage
